9bda4c3e57dbdd474b5baf895dfbeec15606cd21
Lionel Caceres <lkcaceres@Lionel-Caceress-MacBook.local>
2015-11-26 22:13:18 -0800

    Initial Commit

---
+++ Lab7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab7
+//
+//  Created by Lionel Caceres on 11/26/15.
+//  Copyright (c) 2015 Nicholas Caceres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
